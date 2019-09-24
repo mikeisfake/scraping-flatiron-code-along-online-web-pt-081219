66a866f6b36538453482f57c4e4bbbc27743fb5c
Mike Cooper <mikescooper@icloud.com>
2019-09-24 17:37:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,7 @@ class Scraper
   
   def get_courses
     courses = get_page.css(".posts-holder h2")
-    course_array = courses.map{|course| course.text.strip}
-    binding.pry 
+     courses.map{|course| course.text.strip}
   end 
   
   def make_courses
