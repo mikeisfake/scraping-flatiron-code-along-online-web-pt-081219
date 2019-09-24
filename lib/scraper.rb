b875b9e8425d1a4e523760a97023e2a677048aa6
Mike Cooper <mikescooper@icloud.com>
2019-09-24 21:47:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,12 @@ class Scraper
   end 
   
   def make_courses
-  text_array = get_courses.each {|course| Course.new}
+    self.get_courses.each do |post|
+      course = Course.new 
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text 
+    end 
   end 
   
   def print_courses
